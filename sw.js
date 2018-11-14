10669286eb4ca5ed8cfae8930660a960d3a260e2
Yaoning Wu - C15502 <Yaoning.Wu@microchip.com>
2018-11-14 10:50:56 -0700

    fixed service worker

--- sw.js
+++ sw.js
@@ -33,72 +33,54 @@ self.addEventListener('install', event => {
     );
 });
 
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          // IMPORTANT: Clone the request. A request is a stream and
+          // can only be consumed once. Since we are consuming this
+          // once by cache and once by the browser for fetch, we need
+          // to clone the response.
+          var fetchRequest = event.request.clone();
+  
+          return fetch(fetchRequest).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(cacheName)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
+
 // self.addEventListener('fetch', event => {
 //     event.respondWith(
-//       caches.match(event.request).then(response => {
-//         return response || fetch(event.request).then(fetchResponse => { // new
-//           return caches.open(staticCacheName).then(cache => {           // new
-//             cache.put(event.request, fetchResponse.clone());            // new
-//             return fetchResponse;                                       // new
-//           });                                                           // new
-//         });                                                             // new
-//       }).catch(error => {
-//         return new Response('Not connected to the internet', {
-//           status: 404,
-//           statusText: "Not connected to the internet"
-//         });
-//       })
-//     );
-//   });
-
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//       caches.match(event.request)
-//         .then(function(response) {
-//           // Cache hit - return response
-//           if (response) {
-//             return response;
-//           }
-  
-//           // IMPORTANT: Clone the request. A request is a stream and
-//           // can only be consumed once. Since we are consuming this
-//           // once by cache and once by the browser for fetch, we need
-//           // to clone the response.
-//           var fetchRequest = event.request.clone();
-  
-//           return fetch(fetchRequest).then(
-//             function(response) {
-//               // Check if we received a valid response
-//               if(!response || response.status !== 200 || response.type !== 'basic') {
-//                 return response;
-//               }
-  
-//               // IMPORTANT: Clone the response. A response is a stream
-//               // and because we want the browser to consume the response
-//               // as well as the cache consuming the response, we need
-//               // to clone it so we have two streams.
-//               var responseToCache = response.clone();
-  
-//               caches.open(CACHE_NAME)
-//                 .then(function(cache) {
-//                   cache.put(event.request, responseToCache);
-//                 });
-  
-//               return response;
-//             }
-//           );
+//         caches.match(event.request)
+//         .then(response => {
+//             return response || fetch(event.request);
 //         })
-//       );
-//   });
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response => {
-            return response || fetch(event.request);
-        })
-        .catch(error => {
-            console.log(error);
-        })
-    );
-});
+//         .catch(error => {
+//             console.log(error);
+//         })
+//     );
+// });
